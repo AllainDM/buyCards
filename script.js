81a55c639c1c19714992222e94bf0d6d98f45cc3
AllainDM <allaindemoupu@mail.ru>
2021-03-01 16:37:52 +0300

    ИИ. Последняя и перпоследяя карточки. База 5

--- script.js
+++ script.js
@@ -8,16 +8,25 @@ let card;
 let cardsLeft = 5;
 let myWin = 0;
 let oppWin = 0;
+let oppAgr = 1;
+let oppBet;
 
 document.getElementById('cards-left').innerText = 'Осталось карточек: ' + cardsLeft;
+document.getElementById('now-card-value').innerText = 'Текущая карточка была: ' + card;
 document.getElementById('my-points').innerText = 'Мои очки: ' + myPoints;
 document.getElementById('my-coin').innerText = 'Мои монетки: ' + myCoin;
 document.getElementById('opp-points').innerText = 'Очки оппонента: ' + oppPoints;
 document.getElementById('about-opp-coin').innerText = 'Монетки оппонента(макс.): ' + aboutOppCoin;
+document.getElementById('opp-coin').innerText = 'Монетки оппонента(Для теста): ' + oppCoin;
 document.getElementById('my-win').innerText = 'Мои победы: ' + myWin;
 document.getElementById('opp-win').innerText = 'Победы оппонента: ' + oppWin;
 
 function newGame () {
+
+    if (cardsLeft > 0) {
+        alert("Еще остались карточки, пожалуйста доиграйте");
+        return;
+    }
     myCoin = 100;
     myPoints = 0;
     oppCoin = 100;
@@ -30,6 +39,7 @@ function newGame () {
     document.getElementById('my-coin').innerText = 'Мои монетки: ' + myCoin;
     document.getElementById('opp-points').innerText = 'Очки оппонента: ' + oppPoints;
     document.getElementById('about-opp-coin').innerText = 'Монетки оппонента(макс.): ' + aboutOppCoin;
+    document.getElementById('opp-coin').innerText = 'Монетки оппонента(Для теста): ' + oppCoin;
 }
 
 
@@ -45,22 +55,46 @@ function takeCard () {
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
     }
-    cardsLeft -= 1;
-    document.getElementById('cards-left').innerText = 'Осталось карточек: ' + cardsLeft;
-    card = getRandomCard(1,10); 
+    
+    
+    card = getRandomCard(1, 10); 
     answer = +prompt('Выпала карточка на ' + card + ' очка(ов). Сколько вы за нее заплатите?');
-    if (answer > 5) {
+
+    function getRandomAggression(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min; 
+    }
+
+    oppAgr = getRandomAggression(1, 5);
+
+    
+    if (cardsLeft == 1) {
+        oppBet = oppCoin;
+    } else if (cardsLeft == 2 && oppPoints < myPoints) {
+        oppBet = Math.round(0.7 * oppCoin);
+    }
+    
+    else {
+        oppBet = 5;
+    }  
+
+    if (answer > oppBet) {
         myPoints += card;
         document.getElementById('my-points').innerText = 'Мои очки: ' + myPoints;
         myCoin -= answer;
         document.getElementById('my-coin').innerText = 'Мои монетки: ' + myCoin;
+    } else if (answer == oppBet) {
+        alert("Одинаковая ставка, карточка не досталась никому.");
     } else {
         oppPoints += card;
+        oppCoin -= oppBet;
+        document.getElementById('opp-coin').innerText = 'Монетки оппонента(Для теста): ' + oppCoin;
         document.getElementById('opp-points').innerText = 'Очки оппонента: ' + oppPoints;
     }
-    console.log(card);
-    console.log('Мои очки ' + myPoints);
-    console.log('Очки оппонента ' + oppPoints);
+   
+    cardsLeft -= 1;
+    document.getElementById('cards-left').innerText = 'Осталось карточек: ' + cardsLeft;
 
     if (cardsLeft == 0 && myPoints > oppPoints) {
         alert('You WIN!!!');
@@ -71,6 +105,9 @@ function takeCard () {
         oppWin += 1;
         document.getElementById('opp-win').innerText = 'Победы оппонента: ' + oppWin;
     }
+
+    document.getElementById('now-card-value').innerText = 'Текущая карточка была: ' + card;
+    console.log(oppBet);
 
 }
 
